0cda6cd98d1fd95077e9d03cdde79442ca196e24
Halit Uzan <halit.uzan@gmail.com>
2021-12-07 20:21:29 +0300

    first commit

---
+++ js/custom.js
@@ -0,0 +1,29 @@
+// var article = document.getElementsByClassName("sellers-article");
+// var showCart = document.getElementsByClassName("sepet");
+
+// // for(i=0;i<=article.length;i++){
+// //     article[i].addEventListener("mouseover", ()=>{
+// //         showCart.style.display="flex";
+// //         })
+// //     article[i].addEventListener("mouseout", ()=>{
+// //         showCart.style.display="none";
+// //         })
+// // }
+
+// function show(){
+    
+//     var showCart = document.getElementsByClassName("sepet");  
+//     for (i=0;i<=showCart.length;i++){
+//         showCart[i].style.display="flex";
+//     }  
+    
+// }
+// function hide(){
+//     var showCart = document.getElementsByClassName("sepet");  
+//     for (j=0;j<=showCart.length;j++){
+//         showCart[j].style.display="none";
+//     }  
+// }
+// show();
+// hide();
+
