7311059874989c5191b371d0be1042c7aefbc6ef
Halit Uzan <halit.uzan@gmail.com>
2021-12-10 14:12:36 +0300

    Küçük Değişiklikler

--- js/custom.js
+++ js/custom.js
@@ -1,32 +1,3 @@
-// var article = document.getElementsByClassName("sellers-article");
-// var showCart = document.getElementsByClassName("sepet");
-
-// // for(i=0;i<=article.length;i++){
-// //     article[i].addEventListener("mouseover", ()=>{
-// //         showCart.style.display="flex";
-// //         })
-// //     article[i].addEventListener("mouseout", ()=>{
-// //         showCart.style.display="none";
-// //         })
-// // }
-
-// function show(){
-    
-//     var showCart = document.getElementsByClassName("sepet");  
-//     for (i=0;i<=showCart.length;i++){
-//         showCart[i].style.display="flex";
-//     }  
-    
-// }
-// function hide(){
-//     var showCart = document.getElementsByClassName("sepet");  
-//     for (j=0;j<=showCart.length;j++){
-//         showCart[j].style.display="none";
-//     }  
-// }
-// show();
-// hide();
-
 var sellersDetails = document.getElementById("top-main");
 
 fetch('./users.json')
@@ -57,3 +28,35 @@ fetch('./users.json')
 })
 .catch ((error) => console.log("Error"));
 
+
+
+
+
+// var article = document.getElementsByClassName("sellers-article");
+// var showCart = document.getElementsByClassName("sepet");
+
+// // for(i=0;i<=article.length;i++){
+// //     article[i].addEventListener("mouseover", ()=>{
+// //         showCart.style.display="flex";
+// //         })
+// //     article[i].addEventListener("mouseout", ()=>{
+// //         showCart.style.display="none";
+// //         })
+// // }
+
+// function show(){
+    
+//     var showCart = document.getElementsByClassName("sepet");  
+//     for (i=0;i<=showCart.length;i++){
+//         showCart[i].style.display="flex";
+//     }  
+    
+// }
+// function hide(){
+//     var showCart = document.getElementsByClassName("sepet");  
+//     for (j=0;j<=showCart.length;j++){
+//         showCart[j].style.display="none";
+//     }  
+// }
+// show();
+// hide();
